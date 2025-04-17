b99b8b76c6cdd5bba4760af3eae74ddc181dc1fb
William Alvarez <111912100+wilyyyy@users.noreply.github.com>
2025-04-16 23:39:37 -0700

    init formula helpers

---
+++ src/utils/revenueCalculation.ts
@@ -0,0 +1,11 @@
+/**
+ * Calculate new commisison
+ * Formula: Premium × First-Year Commission % × Commission Share % × Confidence Level %
+ */
+export const CalculateNewCommission = () => {}
+
+/**
+ * Calculate trailer commisiion
+ * Formula: Premium × Trailer % × Commission Share % × Confidence Level %
+ */
+export const CalculateTrailerCommision = () => {}
